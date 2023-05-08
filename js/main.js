e535bafba1b86340b87eab6103c0f3f9af0702c2
alex <1678423+alex-codelab@users.noreply.github.com>
2023-05-08 10:59:50 +0200

    send command on Enter

--- js/main.js
+++ js/main.js
@@ -1,45 +1,9 @@
 
-const socket = new WebSocket('ws://192.168.1.105:81');
+
 
 function addData(event){
     document.getElementById("textArea").value += event;
 }
-
-socket.onopen = function(e) {
-  console.log("[open] Connection established");
-  socket.send("?");
-};
-
-socket.onmessage = function(event) {
-    console.log(`[message] Data received from server: ${event.data}`);
-
-      if (event.data instanceof Blob) {
-        reader = new FileReader();
-        reader.onload = () => {
-            console.log("Result: " + reader.result);
-            addData(reader.result);
-        };
-        reader.readAsText(event.data);
-
-    }
-//    else {
-//        console.log("Result: " + event.data);
-//    }
-};
-
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    console.log('[close] Connection died');
-  }
-};
-
-socket.onerror = function(error) {
-  console.log(`[error]`);
-};
 
 function sendCommand(){
     var command_str =  document.getElementById("command_msg")
@@ -55,39 +19,23 @@ function sendCommand2(el, value, axis ){
     socket.send(msgstr);
 }
 
+var input = document.getElementById("command_msg");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("command_msg_btn").click();
+  }
+});
+
+
 function sendHome(el, axis ){
     var msgstr = '$H'+axis +'\n' ;
     socket.send(msgstr);
 }
 
 
-
-function getCirclePart(x, y) {
-  const centerX = 200; // center X coordinate of largest circle
-  const centerY = 200; // center Y coordinate of largest circle
-  const radii = [50, 100, 150, 200]
-  const angle = Math.PI / 4; // angle of rotation (45 degrees in radians)
-  const values = [0.1, 1, 10, 50];
-  var ret = ""
-  const distanceFromCenter = Math.sqrt((x - centerX)**2 + (y - centerY)**2);
-
-  const rotatedX = (x - centerX) * Math.cos(angle) - (y - centerY) * Math.sin(angle) + centerX;
-  const rotatedY = (x - centerX) * Math.sin(angle) + (y - centerY) * Math.cos(angle) + centerY;
-
-
-  if (rotatedX >= centerX && rotatedY < centerY) {
-    ret= "Y";
-  } else if (rotatedX >= centerX && rotatedY >= centerY) {
-    ret = "X";
-  } else if (rotatedX < centerX && rotatedY >= centerY) {
-    ret = "Y-";
-  } else {
-    ret = "X-";
-  }
-
-  for (let i = 0; i < radii.length ; i++) {
-    if (distanceFromCenter < radii[i]) {
-      return ret + values[i]
-    }
-  }
-}
