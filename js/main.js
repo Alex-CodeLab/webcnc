970a3f7f00e2544b4469efdbaa25167a57d65c47
alex <1678423+alex-codelab@users.noreply.github.com>
2023-05-08 14:36:24 +0200

    status websocket. time log.

--- js/main.js
+++ js/main.js
@@ -2,40 +2,21 @@
 
 
 function addData(event){
-    document.getElementById("textArea").value += event;
+    let ev= event.split(/\n/);
+//    console.log("___" +ev)
+    var textarea =  document.getElementById("log_textArea");
+
+    var now = new Date().toLocaleTimeString('en-GB', { hour: "numeric",
+                                             minute: "numeric", second: "numeric"});
+    for (e in ev){
+        if (ev[e] !== ""){
+        console.log(ev[e])
+        textarea.value +=  now + "  " + ev[e] + "\n";
+
+        if(textarea.selectionStart == textarea.selectionEnd) {
+         textarea.scrollTop = textarea.scrollHeight;
+    }
+    }
+    }
 }
-
-function sendCommand(){
-    var command_str =  document.getElementById("command_msg")
-    command_str.value = command_str.value.trim()
-    socket.send(command_str.value);
-    socket.send('\n');
-    console.log(command_str.value);
-}
-
-function sendCommand2(el, value, axis ){
-//    todo: validate input
-    var msgstr = 'G91 G21 F100 '+axis + value +'\n' ;
-    socket.send(msgstr);
-}
-
-var input = document.getElementById("command_msg");
-
-// Execute a function when the user presses a key on the keyboard
-input.addEventListener("keypress", function(event) {
-  // If the user presses the "Enter" key on the keyboard
-  if (event.key === "Enter") {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("command_msg_btn").click();
-  }
-});
-
-
-function sendHome(el, axis ){
-    var msgstr = '$H'+axis +'\n' ;
-    socket.send(msgstr);
-}
-
 
